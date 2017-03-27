1a2595894ea5c279dd39413926db988c26b7e208
Simon\GitHub_Desktop <simon.seo@nyu.edu>
2017-03-27 22:22:28 +0800

    Added cost calculation function

--- Assignment/04_Neural Net/neural_net.py
+++ Assignment/04_Neural Net/neural_net.py
@@ -5,21 +5,21 @@ class NeuralNet():
 	def __init__(self):
 		pass
 
-	def csv_to_list(self, filename):
+	def csv_to_list(self, filename, datatype=float):
 		'''return 1D list saved in csv'''
 		file = open(filename, "r")
 		list = []
 		for line in file:
-			list.append(float(line.strip()))
+			list.append(datatype(line.strip()))
 		file.close()
 		return list
 
-	def csv_to_matrix(self, filename):
+	def csv_to_matrix(self, filename, datatype=float):
 		'''return 2D matrix saved in csv'''
 		file = open(filename, "r")
 		matrix = []
 		for line in file:
-			matrix.append([float(el) for el in line.strip().split(",")])
+			matrix.append([datatype(el) for el in line.strip().split(",")])
 		file.close()
 		return matrix
 
@@ -72,6 +72,30 @@ class NeuralNet():
 			if label[i] != classifier(data[i], *weight_matrix_args): error_count += 1
 		return error_count/m
 
+	def _cost_MLE(self, prediction, label):
+		'''maximum likelihood error: -(y*log(h) + (1-y)log(1-h))'''
+		h, y = prediction, label
+		return -(y * math.log(h) + (1 - y) * math.log(1 - h))
+
+	def _cost_MSE(self, prediction, label):
+		'''mean squared error: (h-y)**2 / 2'''
+		h, y = prediction, label
+		return (h-y)**2 / 2
+
+	def cost(self, output_matrix, label_list, cost_model):
+		'''calculates cost for given outputs, labels, and cost model'''
+		m = len(output_matrix) # number of datasets/predictions/labels
+		K = len(output_matrix[0]) # number of classes
+		sum_total = 0
+		for i in range(m):
+			h = output_matrix[i]
+			y = [0] * K
+			y[label_list[i]] = 1
+			sum_dataset = 0
+			for k in range(K):
+				sum_dataset += cost_model(h[k], y[k])
+			sum_total += sum_dataset
+		return sum_total/m
 
 def main():
 	nn = NeuralNet()
@@ -79,10 +103,14 @@ def main():
 	data = nn.csv_to_matrix("ps5_data.csv")
 	w1 = nn.csv_to_matrix("ps5_theta1.csv")
 	w2 = nn.csv_to_matrix("ps5_theta2.csv")
-	label = nn.csv_to_list("ps5_data-labels_edit.csv")
+	label = nn.csv_to_list("ps5_data-labels_edit.csv", int)
+	
+	print("Classified input image as:", nn.classifier_idx(data[0], w1, w2))
+
 	# print("error rate:", nn.error(data, label, nn.classifier_idx, w1, w2)) # error rate = 2.48%
-
-
+	
+	# outputs = [nn.forward_propagate(data[i], w1, w2) for i in range(len(data))]
+	# print("J:", nn.cost(outputs, label, nn._cost_MLE)) # J = 0.28762951217843125
 
 if __name__ == '__main__':
 	main()
